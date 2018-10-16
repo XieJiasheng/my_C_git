61ec0185f45e917e6cadc7d441dc29fb3e587de4
Jiasheng.Xie <jiasheng.xie@gzseeing.com>
2018-10-16 11:41:50 +0800

    feat: add shell code to test shell variable

---
+++ shell_test/shell_variable.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# to test shell variable 
+
+your_name="luccy"
+age=21
+# the double quotes usage
+echo "your name is ${your_name}"
+echo "your age is ${age}\""
+#get the length of string
+echo "the length of name is ${#your_name}"
+echo "the substring of name is ${your_name:1:3}"
+# the single quotes usage
+person_info='name is '${your_name}', age is '${age}
+echo ${person_info}
+# back quotes usage
+for file_info in `ls `; do
+    echo 'the file information is '${file_info}
+done
+
+:<<doc
+this is a comment1
+this is a comment2
+doc
+
+:<<!
+this is a comment3
+this is a comment4
+!
+
+:<<'
+this is a comment5
+this is a comment6
+'
+
+# find substring index
+string="hello world"
+echo `expr index "${string}" ed`
+# find substring
+echo `expr substr "${string}" 7 11`
+
+# array
+array_num=("one" "two" "three")
+array_num[3]="four"
+array_num[5]="six"
+echo "all value of array:${array_num[@]}"
+echo "length of array:${#array_num[*]}"
+echo "one of array:${array_num[3]}"
+echo "one of array:${array_num[4]}"
